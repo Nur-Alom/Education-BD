974c5c4f1e12f35e6811d671920914f3c8a92906
Nur-Alom <alamn7150@gmail.com>
2021-10-04 19:02:18 +0600

    last commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,10 +5,6 @@ const Footer = () => {
         <div className="bg-dark text-white py-4">
             <h1>EducationBD <i className="fas fa-graduation-cap"></i></h1>
             <p>All Rights Reserved. EducationBD Online ©2021.</p>
-            <i className="fs-3 p-3 text-primary fab fa-facebook"></i>
-            <i className="fs-3 p-3 text-info fab fa-twitter-square"></i>
-            <i className="fs-3 p-3 text-danger fab fa-instagram"></i>
-            <i className="fs-3 p-3 text-success fab fa-chrome"></i>
         </div>
     );
 };
